312127d23b4253910217981b8e93ffc14a76f463
Minna <minna1025@gmail.com>
2022-02-23 23:28:07 +0900

    Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,53 +1,17 @@
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1,
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8,
-  },
-  {
-    id: 2,
-    name: "The Godfather I",
-    score: 99,
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2,
-  },
-];
+import fetch from "cross-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-};
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
 
-const findLastId = () => {
-  return movies[movies.length - 1].id;
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: findLastId() + 1,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
